b4402d75b0e2888a9233ced3c7f769e21c43e0b1
payalpayaldhaker <90671274+payalpayaldhaker@users.noreply.github.com>
2021-11-17 23:21:20 -0800

    php4

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php  echo 'php4';  ?></title>
+        <style>
+        
+        
+        </style>
+    </head>
+    <body>
+
+            <?php 
+                    echo 'payal dhakad'.'   '.'neemuch'.'  '.'payal';
+                    
+                    
+            ?>
+
+
+            <script>
+            
+             </script>
+    </body>
+    </html>
+        <?php  
+                $name ='dhakad';
+               
+                
+                
+
+        ?>
+
+
